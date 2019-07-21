8c52880b7936aab9ff8bae5d45690e8e6c6fc4ce
IamZenNinja <52015776+iamzenninja@users.noreply.github.com>
2019-07-21 11:25:41 -0400

    Update README.sh

--- README.sh
+++ README.sh
@@ -1,12 +1,14 @@
-# global-linux-helper
-A simpler way to keep and take automation knowledge with me as I go
+# start with ~/.bashrc or ~/.zshrc
+alias mm="micro ~/.mrc;echo '!@#$>mmm=refresh'";alias mmm=". ~/.mrc";. ~/.mrc;
+
+# CONTENTS OF ~/.mrc
+# Start below here:
 
 # .bashrc stuff
-alias mm="micro ~/.mrc;echo '!@#$>mmm=refresh'";alias mmm=". ~/.mrc";. ~/.mrc;
 alias m="micro"
 alias si="sudo snap install"
 alias ai="sudo apt install"
 
 # help
-alias tt-file="echo 'lfm, vifm, ranger'"
+alias tt-file="echo 'lfm, vifm, ranger, hh'"
 alias tt-edit="echo 'vi, micro, slap'"
