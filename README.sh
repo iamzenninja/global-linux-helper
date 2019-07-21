35913002d1ce50f96089b6ad2e4c4c107ec43ec4
IamZenNinja <52015776+iamzenninja@users.noreply.github.com>
2019-07-21 11:23:26 -0400

    Create README.sh

---
+++ README.sh
@@ -0,0 +1,12 @@
+# global-linux-helper
+A simpler way to keep and take automation knowledge with me as I go
+
+# .bashrc stuff
+alias mm="micro ~/.mrc;echo '!@#$>mmm=refresh'";alias mmm=". ~/.mrc";. ~/.mrc;
+alias m="micro"
+alias si="sudo snap install"
+alias ai="sudo apt install"
+
+# help
+alias tt-file="echo 'lfm, vifm, ranger'"
+alias tt-edit="echo 'vi, micro, slap'"
